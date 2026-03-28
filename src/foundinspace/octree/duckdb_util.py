485659a035c1c626f5588d472688583bce2de017
Kaj Siebert <kaj@k-si.com>
2026-03-28 16:01:52 +0100

    Add PRESERVE_INSERTION_ORDER option

--- src/foundinspace/octree/duckdb_util.py
+++ src/foundinspace/octree/duckdb_util.py
@@ -6,9 +6,10 @@ TEMP_DIR = config('DUCKDB_TEMP_DIR', default=None)
 MAX_TEMP_DIRECTORY_SIZE = config('DUCKDB_MAX_TEMP_DIRECTORY_SIZE', default=None)
 MEMORY_LIMIT = config('DUCKDB_MEMORY_LIMIT', default=None)
 THREADS = config('DUCKDB_THREADS', default=None)
+PRESERVE_INSERTION_ORDER = config('DUCKDB_PRESERVE_INSERTION_ORDER', default=None)
 
 
-def configure_connection(con: duckdb.DuckDBPyConnection) -> None:   
+def configure_connection(con: duckdb.DuckDBPyConnection) -> None:
     if TEMP_DIR is not None:
         con.execute("SET temp_directory = ?", [TEMP_DIR])
         print(f"SET temp_directory = {TEMP_DIR}")
@@ -21,3 +22,6 @@ def configure_connection(con: duckdb.DuckDBPyConnection) -> None:
     if THREADS is not None:
         con.execute("SET threads = ?", [THREADS])
         print(f"SET threads = {THREADS}")
+    if PRESERVE_INSERTION_ORDER is not None:
+        con.execute("SET preserve_insertion_order = ?", [PRESERVE_INSERTION_ORDER])
+        print(f"SET preserve_insertion_order = {PRESERVE_INSERTION_ORDER}")
